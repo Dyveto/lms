746408bb7f5b985a46b519b572e3a224d3bf2918
Jussi Torres <jussitorresgonzalez@gmail.com>
2026-03-25 22:18:51 -0500

    fix: configure MapStruct annotation processor and Spring component model for AssessmentMapper

--- src/main/java/edu/unimagdalena/lms/services/mapper/AssessmentMapper.java
+++ src/main/java/edu/unimagdalena/lms/services/mapper/AssessmentMapper.java
@@ -1,20 +1,19 @@
 package edu.unimagdalena.lms.services.mapper;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import edu.unimagdalena.lms.dto.request.AssessmentRequestDto;
 import edu.unimagdalena.lms.dto.response.AssessmentResponseDto;
 import edu.unimagdalena.lms.entities.Assessment;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface AssessmentMapper {
-
-    @Mapping(source = "student.id", target = "studentId")
-    @Mapping(source = "course.id", target = "courseId")
-    AssessmentResponseDto toDto(Assessment assessment);
-
+    
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "student", ignore = true)
     @Mapping(target = "course", ignore = true)
+    @Mapping(target = "takenAt", ignore = true)
     Assessment toEntity(AssessmentRequestDto assessmentRequestDto);
+
+    AssessmentResponseDto toDto(Assessment assessment);
 }
